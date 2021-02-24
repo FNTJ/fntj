eb4b5224f3ece2301d27922a686ae5d9b779822f
Hojin <cacabi24@hanmail.net>
2021-02-24 17:27:08 +0900

    17:27

--- mbeti/src/main/java/com/mbeti/controller/MemberController.java
+++ mbeti/src/main/java/com/mbeti/controller/MemberController.java
@@ -1,11 +1,17 @@
 package com.mbeti.controller;
 
+import java.util.Random;
+
 import javax.inject.Inject;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -36,6 +42,8 @@ public class MemberController {
    @Inject
    FreeBoardService fbservice;
    
+   @Autowired
+   private JavaMailSender mailSender;	
    
 // 회원가입 get
 @RequestMapping(value = "/user/register", method = RequestMethod.GET)
@@ -210,6 +218,52 @@ public String memberDelete(MemberVO vo, HttpSession session, RedirectAttributes 
       
    }
    
-   
+   /* 이메일 인증 */
+	@RequestMapping(value="/mailCheck", method=RequestMethod.GET)
+	@ResponseBody
+	public String mailCheckGET(String email) throws Exception{
+		
+		/* 뷰(View)로부터 넘어온 데이터 확인 */
+		logger.info("이메일 데이터 전송 확인");
+		logger.info("이메일 : " + email);
+				
+		/* 인증번호(난수) 생성 */
+		Random random = new Random();
+		int checkNum = random.nextInt(888888) + 111111;
+		logger.info("인증번호 " + checkNum);
+		
+		/* 이메일 보내기 */
+		String setFrom = "fntjambox@gmail.com";
+		String toMail = email;
+		String title = "회원가입 인증 이메일 입니다."; // 메일제목
+		String content = 						// 메일내용
+				"홈페이지를 방문해주셔서 감사합니다." +
+				"<br><br>" + 
+				"인증 번호는 " + checkNum + "입니다." + 
+				"<br>" + 
+				"해당 인증번호를 인증번호 확인란에 기입하여 주세요.";		
+		
+
+		try {
+			
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+			helper.setFrom(setFrom);
+			helper.setTo(toMail);
+			helper.setSubject(title);
+			helper.setText(content,true);
+			mailSender.send(message);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}		
+		
+		String num = Integer.toString(checkNum);
+		
+		return num;
+		
+	}
+
+
    
 }
